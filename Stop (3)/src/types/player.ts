172e12a028bb144073696ea137097a532f640b13
jaime <75831652+jdorta2206@users.noreply.github.com>
2025-07-24 14:00:28 +0200

    Delete Stop (3) directory
    Signed-off-by: jaime <75831652+jdorta2206@users.noreply.github.com>

--- Stop (3)/src/types/player.ts
+++
@@ -1,9 +0,0 @@
-// src/types/player.ts
-export interface PlayerInLobby {
-  id: string;
-  name: string;
-  // Añade otras propiedades que necesites
-  score?: number;
-  isHost?: boolean;
-  // etc...
-}
