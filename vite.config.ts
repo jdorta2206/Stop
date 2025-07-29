ccbd2ce37be55a16d21156395a8459374247ee2d
MGX <admin@mgx.dev>
2025-07-28 19:23:19 -0700

    Updated vite.config.ts for environment variable compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  define: {
+    'process.env': import.meta.env, // Opcional, para compatibilidad
+  },
   plugins: [react()],
   base: '/', // Ruta base para Netlify
   resolve: {
